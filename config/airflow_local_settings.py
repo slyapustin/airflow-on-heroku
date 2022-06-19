50f8eaf4667ff4d9042871941402fd56a0425531
Sergey Lyapustin <s.lyapustin@gmail.com>
2022-06-19 22:53:17 +0200

    Added some hacky logs.

--- config/airflow_local_settings.py
+++ config/airflow_local_settings.py
@@ -1,4 +1,5 @@
 import os
 
+print('!!!!Changing SQL_ALCHEMY_CONN!!!')
 # This is to fix SQLAlchemy issue: https://github.com/sqlalchemy/sqlalchemy/issues/6083
 SQL_ALCHEMY_CONN = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1)
