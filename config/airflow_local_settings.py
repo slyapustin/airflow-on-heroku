23ee974848d906e1133fa5272e444fbbe7d199d3
Sergey Lyapustin <s.lyapustin@gmail.com>
2022-06-19 23:01:22 +0200

    Well, we need to reinit Session as local settings loaded too late.

--- config/airflow_local_settings.py
+++ config/airflow_local_settings.py
@@ -1,5 +1,11 @@
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
 
 print('!!!!Changing SQL_ALCHEMY_CONN!!!')
 # This is to fix SQLAlchemy issue: https://github.com/sqlalchemy/sqlalchemy/issues/6083
 SQL_ALCHEMY_CONN = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(SQL_ALCHEMY_CONN, **engine_args)
+Session = scoped_session(
+    sessionmaker(autocommit=False, autoflush=False, bind=engine))
