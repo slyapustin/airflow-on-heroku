d84582a5fe67f2828c20d2ddff5569c4d1bf73b8
Sergey Lyapustin <s.lyapustin@gmail.com>
2022-06-21 23:51:50 +0200

    We don't need that workaraund for now.

--- config/airflow_local_settings.py
+++
@@ -1,7 +0,0 @@
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session, sessionmaker
-
-print('!!!!Changing SQL_ALCHEMY_CONN!!!')
-# This is to fix SQLAlchemy issue: https://github.com/sqlalchemy/sqlalchemy/issues/6083
-globals()['SQL_ALCHEMY_CONN'] = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://", 1)
